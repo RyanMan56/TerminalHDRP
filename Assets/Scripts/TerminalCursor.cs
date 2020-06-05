c95add9cade2d00c71c218c5192b462f9f61f58e
Ryan Jones <RyanMan56@gmail.com>
2020-06-05 23:22:22 +0100

    Added click sounds, folders, text and selection boxes!

---
+++ Assets/Scripts/TerminalCursor.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalCursor : MonoBehaviour
+{
+    public Transform bottomLeft;
+    public Transform topRight;
+    private float mouseSpeed = 2.75f;
+    public AudioSource clickIn1;
+    public AudioSource clickOut1;
+    public AudioSource clickIn2;
+    public AudioSource clickOut2;
+    public SpriteRenderer highlight;
+
+    public Vector3? mouseDownPosition = null; // world position
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Transform MoveCursor()
+    {
+        float dx = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
+        float dy = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
+
+        transform.Translate(new Vector3(dx * mouseSpeed, dy * mouseSpeed, 0), Space.Self);
+        float newX = transform.localPosition.x > topRight.localPosition.x
+            ? topRight.localPosition.x
+            : transform.localPosition.x < bottomLeft.localPosition.x
+                ? bottomLeft.localPosition.x
+                : transform.localPosition.x;
+        float newY = transform.localPosition.y > topRight.localPosition.y
+            ? topRight.localPosition.y
+            : transform.localPosition.y < bottomLeft.localPosition.y
+                ? bottomLeft.localPosition.y
+                : transform.localPosition.y;
+        Vector3 newLocalPos = new Vector3(newX, newY, transform.localPosition.z);
+        if (!transform.localPosition.Equals(newLocalPos))
+        {
+            transform.localPosition = newLocalPos;
+        }
+
+        return transform;
+    }
+
+    public void Click()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            bool randomBool = (Random.value > 0.5f);
+            if (randomBool)
+            {
+                clickIn1.Play();
+            }
+            else
+            {
+                clickIn2.Play();
+            }
+        }
+        if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
+        {
+            bool randomBool = (Random.value > 0.5f);
+            if (randomBool)
+            {
+                clickOut1.Play();
+            }
+            else
+            {
+                clickOut2.Play();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseDownPosition = transform.position; // world position
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseDownPosition = null;
+            highlight.size = Vector2.zero;
+        }
+    }
+
+    public void Drag()
+    {
+        Vector3 distance = transform.position - mouseDownPosition.Value;
+        highlight.size = new Vector2(distance.x / highlight.transform.localScale.x, distance.y / highlight.transform.localScale.y);
+        highlight.transform.localPosition = transform.localPosition - new Vector3(distance.x / 2, distance.y / 2);
+    }
+}
--- Assets/Scripts/TerminalUI.cs
+++ Assets/Scripts/TerminalUI.cs
@@ -9,8 +9,7 @@ public class TerminalUI : MonoBehaviour
     private float width;
     private float height;
     public bool active = false;
-    public Transform cursor;
-    private float mouseSpeed = 0.5f;
+    public TerminalCursor cursor;
 
 
     // Start is called before the first frame update
@@ -25,7 +24,12 @@ public class TerminalUI : MonoBehaviour
     {
         if (active)
         {
-            MoveCursor();
+            cursor.MoveCursor();
+            cursor.Click();
+            if (cursor.mouseDownPosition.HasValue)
+            {
+                cursor.Drag();
+            }
         }
     }
 
@@ -33,37 +37,11 @@ public class TerminalUI : MonoBehaviour
         active = isUsing;
     }
 
-    private void MoveCursor()
-    {
-        float dx = Input.GetAxis("Mouse X") * mouseSpeed;
-        float dy = Input.GetAxis("Mouse Y") * mouseSpeed;
-
-        cursor.Translate(new Vector3(dx * mouseSpeed, dy * mouseSpeed, 0), Space.Self);
-        float newX = cursor.localPosition.x > topRight.localPosition.x
-            ? topRight.localPosition.x
-            : cursor.localPosition.x < bottomLeft.localPosition.x
-                ? bottomLeft.localPosition.x
-                : cursor.localPosition.x;
-        float newY = cursor.localPosition.y > topRight.localPosition.y
-            ? topRight.localPosition.y
-            : cursor.localPosition.y < bottomLeft.localPosition.y
-                ? bottomLeft.localPosition.y
-                : cursor.localPosition.y;
-        Vector3 newLocalPos = new Vector3(newX, newY, cursor.localPosition.z);
-        if (!cursor.localPosition.Equals(newLocalPos))
-        {
-            cursor.localPosition = newLocalPos;
-        }
-    }
-
 
     Vector2 WorldToNormalized(Vector2 world)
     {
-        Debug.Log($"world: {world}");
         float normalizedX = (world.x - bottomLeft.localPosition.x) / (topRight.localPosition.x - bottomLeft.localPosition.x);
         float normalizedY = (world.y - bottomLeft.localPosition.y) / (topRight.localPosition.y - bottomLeft.localPosition.y);
-
-        Debug.Log($"nX: {normalizedX}, nY: {normalizedY}");        
 
         return new Vector2(normalizedX, normalizedY);
     }
